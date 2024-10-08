dd6c53de5cde0a1532a016953aa78789614dd42c
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-10-08 04:19:58 -0700

    generateLoadShape now part of helperFunctions.jl, which itself is a module now

--- src/parsePVData.jl
+++ src/parsePVData.jl
@@ -4,68 +4,27 @@ module parsePVData
 
 export parse_pv_data
 
-function parse_pv_data(systemName::String, T::Int; LoadShape=nothing)
+include("helperFunctions.jl")
+using .helperFunctions: generateLoadShape
 
+function parse_pv_data(systemName::String, T::Int; LoadShape=nothing, filenameLoadShape=nothing)
     # get wd: the path of <this> file
     wd = @__DIR__
     # Construct the file path using wd
     filename = joinpath(wd, "..", "rawData", systemName, "PVSystem.dss")
 
     # Initialize data structures
-    Dset = Set{Int}()                    # Set of nodes with PVs
-    p_D_R = Dict{Int,Float64}()        # Rated PV active power (Pmpp, kW)
-    S_D_R = Dict{Int,Float64}()        # Rated PV apparent power (kVA)
-    irrad = Dict{Int,Float64}()        # Irradiance values for each PV
-    p_D = Dict{Int,Vector{Float64}}()  # PV active power profile over time
+    Dset = Set{Int}()                     # Set of nodes with PVs
+    p_D_R = Dict{Int,Float64}()         # Rated PV active power (Pmpp, kW)
+    S_D_R = Dict{Int,Float64}()         # Rated PV apparent power (kVA)
+    irrad = Dict{Int,Float64}()         # Irradiance values for each PV
+    p_D = Dict{Int,Vector{Float64}}()   # PV active power profile over time
 
-    LoadShapePV = []  # This will hold the T-values after scaling
+    LoadShapePV = LoadShape 
 
-    # LoadShape default file if LoadShape is not provided
+    # If the user doesn't provide a LoadShape, generate it using the helper function
     if LoadShape === nothing
-        loadshape_filename = joinpath(wd, "..", "rawData", systemName, "LoadShapePVDefault.dss")
-        defaultLoadShape = []
-
-        # Read LoadShapePVDefault.dss
-        open(loadshape_filename, "r") do file
-            for line in eachline(file)
-                # Remove comments and strip whitespace
-                line = split(line, "!")[1]
-                line = strip(line)
-                if isempty(line)
-                    continue
-                end
-                # Parse the load shape values
-                if occursin("mult=[", line)
-                    shape_str = strip(split(line, "mult=")[2], ['[', ']'])
-                    defaultLoadShape = [parse(Float64, val) for val in split(shape_str)]
-                end
-            end
-        end
-
-
-        # Scale or subsample the load shape based on T
-        if T < 24
-            # Take the middle T values
-            start_idx = Int(floor((24 - T) / 2)) + 1
-            LoadShapePV = defaultLoadShape[start_idx:(start_idx+T-1)]
-        elseif T > 24
-            # Supersample to generate T values
-            for i in 1:T
-                # Use a basic supersampling method by linearly interpolating between adjacent values
-                scaled_idx = (i - 1) * (24 - 1) / (T - 1) + 1
-                lower_idx = Int(floor(scaled_idx))
-                upper_idx = min(lower_idx + 1, 24)
-                frac = scaled_idx - lower_idx
-                interpolated_value = (1 - frac) * defaultLoadShape[lower_idx] + frac * defaultLoadShape[upper_idx]
-                push!(LoadShapePV, interpolated_value)
-            end
-        else
-            # T == 24, use the default load shape
-            LoadShapePV = defaultLoadShape
-        end
-
-        # Now set LoadShape to this scaled result for the rest of the function
-        LoadShape = LoadShapePV
+        LoadShapePV = generateLoadShape(T, filenameLoadShape=filenameLoadShape)
     end
 
     # Open and read the PVSystem.dss file
@@ -127,14 +86,15 @@ function parse_pv_data(systemName::String, T::Int; LoadShape=nothing)
                     irrad[j] = 1.0  # Default irradiance if not specified
                 end
 
-                # Calculate active power profile over time using LoadShape
-                p_D[j] = [p_D_R[j] * LoadShape[t] for t in 1:T]
+                # Calculate active power profile over time using the user-provided or generated LoadShapePV
+                p_D[j] = [p_D_R[j] * LoadShapePV[t] for t in 1:T]
             end
         end
     end
 
     n_D = length(Dset)
 
+    # Return the extracted data as a dictionary
     pvData = Dict(
         :n_D => n_D,
         :Dset => Dset,
@@ -142,12 +102,10 @@ function parse_pv_data(systemName::String, T::Int; LoadShape=nothing)
         :S_D_R => S_D_R,
         :irrad => irrad,
         :p_D => p_D,
-        :LoadShapePV => LoadShapePV  # Add the scaled load shape to the output
+        :LoadShapePV => LoadShapePV  # Store the LoadShapePV used (either user-supplied or generated)
     )
 
-    # Return the extracted data as a dictionary
     return pvData
-
 end
 
 end # module
