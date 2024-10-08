eaf41ff9e090d9d32a4521525e6d8a909c140475
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-10-08 02:22:14 -0700

    more necessary oargs added

--- src/parseBranchData.jl
+++ src/parseBranchData.jl
@@ -5,6 +5,8 @@ module parseBranchData
 export parse_branch_data
 
 function parse_branch_data(systemName::String)
+
+    # Todo: Ensure that substation bus being equal to 1 is not taken for granted, have some kwarg or something to ensure that even bus 153 can be the substation bus
     
     wd = @__DIR__
     # Construct the file path using wd
@@ -17,6 +19,12 @@ function parse_branch_data(systemName::String)
     xdict = Dict{Tuple{Int,Int},Float64}()  # Reactance of each branch
     parent = Dict{Int,Int}()                 # parent node of each node
     children = Dict{Int,Vector{Int}}()       # children nodes of each node
+
+    # Initialize additional sets and parameters
+    N1set = Set{Int}()                       # Buses connected to substation bus (1)
+    Nm1set = Set{Int}()                      # Buses not connected to substation bus (1)
+    L1set = Set{Tuple{Int,Int}}()           # Branches where one node is 1
+    Lm1set = Set{Tuple{Int,Int}}()          # Branches where no node is 1
 
     # Regular expression to match key=value pairs with optional spaces
     kv_pattern = r"(\w+)\s*=\s*([\S]+)"
@@ -69,6 +77,18 @@ function parse_branch_data(systemName::String)
                     else
                         children[from_bus] = [to_bus]
                     end
+
+                    # Update N1set, Nm1set, L1set, Lm1set
+                    if from_bus == 1
+                        push!(N1set, to_bus)
+                        push!(L1set, (from_bus, to_bus))
+                    elseif to_bus == 1
+                        push!(N1set, from_bus)
+                        push!(L1set, (from_bus, to_bus))
+                    else
+                        Nm1set = union(Nm1set, [from_bus, to_bus])
+                        push!(Lm1set, (from_bus, to_bus))
+                    end
                 else
                     error("Bus1 or Bus2 not specified for a line in BranchData.dss")
                 end
@@ -93,6 +113,12 @@ function parse_branch_data(systemName::String)
     N = length(Nset)
     m = length(Lset)
 
+    # Calculate additional parameters
+    N1 = length(N1set)
+    Nm1 = length(Nm1set)
+    m1 = length(L1set)
+    mm1 = length(Lm1set)
+
     # Create a dictionary with all outputs
     branchData = Dict(
         :Nset => Nset,
@@ -103,10 +129,19 @@ function parse_branch_data(systemName::String)
         :children => children,
         :N => N,
         :m => m,
+        :N1set => N1set,
+        :Nm1set => Nm1set,
+        :L1set => L1set,
+        :Lm1set => Lm1set,
+        :N1 => N1,
+        :Nm1 => Nm1,
+        :m1 => m1,
+        :mm1 => mm1
     )
 
     return branchData
 
-end 
+end
+
 
 end # module
