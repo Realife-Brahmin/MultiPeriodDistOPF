b922cb41188cfd3deb26eee36d9c2fae8ddcebf9
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-10-11 15:33:27 -0700

    hmm first run infeasible. Need to investigate

--- solveMPOPF.jl
+++ solveMPOPF.jl
@@ -382,7 +382,7 @@ C, η_C, η_D = LoadShapeCost, eta_C, eta_D
 # Initializing Variables
 # ===========================
 
-@unpack Tset, V_Subs, B0 = data;
+@unpack Tset = data;
 # Initialize substation power flow variable
 for t in Tset
     set_start_value(P_Subs[t], 0.0)
