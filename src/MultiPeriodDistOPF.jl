ebe6b9ece7e2b356ec0298ef49259c51e51cc75f
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-10-22 06:57:03 -0700

    dunno what's the issue wiht helperFunctions module

--- src/MultiPeriodDistOPF.jl
+++ src/MultiPeriodDistOPF.jl
@@ -1,5 +1,10 @@
 module MultiPeriodDistOPF
 
+# include("./helperFunctions.jl")
+# using .helperFunctions: myprintln
+# include("./helperFunctions.jl")
+# using .helperFunctions
+
 include("./parseOpenDSSFiles.jl")
 using .parseOpenDSSFiles
 
@@ -13,4 +18,4 @@ using .Exporter: export_decision_variables
 # Re-export the function from parseOpenDSSFiles
 export export_decision_variables, myprintln, parse_all_data, parse_system_simulation_data, parse_branch_data, parse_load_data, parse_pv_data, parse_battery_data, evaluate_voltage_limits, generateBinaryLoadShape, plot_battery_actions
 
-end
+end # module
