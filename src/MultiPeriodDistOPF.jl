03b6f8cd0bce2768a0f22e7e276ad51be2fb0b03
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-10-23 19:54:47 -0700

    functionRetriever module now stores all functions which can be used on top of the optimization results for computing any desired outputs

--- src/MultiPeriodDistOPF.jl
+++ src/MultiPeriodDistOPF.jl
@@ -6,6 +6,10 @@ module MultiPeriodDistOPF
 include("./computeOutputs.jl")
 using .computeOutputs: compute_output_values
 
+include("./functionRetriever.jl")
+# using .functionRetriever: get_scd
+using .functionRetriever
+
 include("./helperFunctions.jl")
 using .helperFunctions: myprintln
 
@@ -20,6 +24,6 @@ include("./exporter.jl")
 using .Exporter: export_decision_variables
 
 # Re-export the function from parseOpenDSSFiles
-export compute_output_values, export_decision_variables, myprintln, parse_all_data, parse_system_simulation_data, parse_branch_data, parse_load_data, parse_pv_data, parse_battery_data, evaluate_voltage_limits, generateBinaryLoadShape, plot_battery_actions
+export compute_output_values, export_decision_variables, get_scd, myprintln, parse_all_data, parse_system_simulation_data, parse_branch_data, parse_load_data, parse_pv_data, parse_battery_data, evaluate_voltage_limits, generateBinaryLoadShape, plot_battery_actions
 
 end # module
---
+++ src/functionRetriever.jl
@@ -0,0 +1,88 @@
+module functionRetriever
+
+export get_real_power_loss, get_substation_power, get_substation_power_cost, get_scd, get_terminal_SOC_violation
+
+import JuMP: value  # Importing JuMP's value function to extract values from the model
+using Parameters: @unpack  # For easier unpacking of parameters from data
+
+# Function to get real power losses from a model
+function get_real_power_loss(model, data; horizon::String="allT")
+    @unpack Tset, Lset, rdict_pu, kVA_B = data
+    P = model[:P]
+    l = model[:l]
+
+    if horizon == "1toT"
+        loss_vs_t_kW = [kVA_B * sum(rdict_pu[i, j] * value(l[(i, j), t]) for (i, j) in Lset) for t in Tset]
+        return loss_vs_t_kW
+    elseif horizon == "allT"
+        total_loss_kW = kVA_B * sum(rdict_pu[i, j] * value(l[(i, j), t]) for (i, j) in Lset, t in Tset)
+        return total_loss_kW
+    else
+        error("Specify either '1toT' or 'allT'")
+    end
+end
+
+# Function to get substation power in kW
+function get_substation_power(model, data; horizon::String="allT")
+    @unpack Tset, kVA_B = data
+    P_Subs = model[:P_Subs]
+
+    if horizon == "1toT"
+        substation_power_vs_t_kW = [value(P_Subs[t]) * kVA_B for t in Tset]
+        return substation_power_vs_t_kW
+    elseif horizon == "allT"
+        total_substation_power_kW = sum(value(P_Subs[t]) * kVA_B for t in Tset)
+        return total_substation_power_kW
+    else
+        error("Specify either '1toT' or 'allT'")
+    end
+end
+
+# Function to get substation power cost in dollars
+function get_substation_power_cost(model, data; horizon::String="allT")
+    @unpack Tset, LoadShapeCost, delta_t, kVA_B = data
+    P_Subs = model[:P_Subs]
+
+    if horizon == "1toT"
+        substation_cost_vs_t_dollar = [LoadShapeCost[t] * value(P_Subs[t]) * kVA_B * delta_t for t in Tset]
+        return substation_cost_vs_t_dollar
+    elseif horizon == "allT"
+        total_substation_cost_dollar = sum(LoadShapeCost[t] * value(P_Subs[t]) * kVA_B * delta_t for t in Tset)
+        return total_substation_cost_dollar
+    else
+        error("Specify either '1toT' or 'allT'")
+    end
+end
+
+# Function to get SCD (State of Charge Difference) in kW
+function get_scd(model, data; horizon::String="allT")
+    @unpack Tset, Bset, kVA_B = data
+    P_c = model[:P_c]
+    P_d = model[:P_d]
+
+    if horizon == "1toT"
+        scd_vs_t_kW = [kVA_B * sum(max(value(P_c[j, t]), value(P_d[j, t])) - abs(value(P_c[j, t]) - value(P_d[j, t])) for j in Bset) for t in Tset]
+        return scd_vs_t_kW
+    elseif horizon == "allT"
+        total_scd_kW = kVA_B * sum(max(value(P_c[j, t]), value(P_d[j, t])) - abs(value(P_c[j, t]) - value(P_d[j, t])) for j in Bset, t in Tset)
+        return total_scd_kW
+    else
+        error("Specify either '1toT' or 'allT'")
+    end
+end
+
+# Function to get terminal SOC violation in kWh
+function get_terminal_SOC_violation(model, data)
+    @unpack T, Bset, Bref_pu, kVA_B = data
+    B = model[:B]
+
+    soc_violation_kWh = kVA_B * sum(abs(value(B[j, T]) - Bref_pu[j]) for j in Bset)
+    return soc_violation_kWh
+end
+
+# Function to get any variable from the model (does not depend on horizon)
+function get_variable(model, variable_name::Symbol)
+    return model[variable_name]  # Fetch the variable using its symbol
+end
+
+end # module FunctionRetriever
