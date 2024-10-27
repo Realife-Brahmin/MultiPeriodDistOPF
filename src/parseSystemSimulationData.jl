285c49d40cae2bf5e0553614e40947bdacc2a321
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-10-26 21:01:41 -0700

    using sorted Tset was a good decision

--- src/parseSystemSimulationData.jl
+++ src/parseSystemSimulationData.jl
@@ -77,10 +77,9 @@ function parse_system_simulation_data(systemName::String, T::Int;
     MVA_B = kVA_B/1000
     Z_B = (kV_B)^2/MVA_B
     
-    # Saving Horizon Duration here
-    # Tset = Set(1:T)
     Tset = collect(1:T)
 
+    
     sysSimData = Dict(
         :alpha => alpha, # user input
         :systemName => systemName, # user input
