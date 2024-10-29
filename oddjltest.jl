01a1288364429f7ff1b3617929113b34628f2b93
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-10-29 15:37:44 -0700

    initial testing of odd.jl with `ads10_1ph` seems successful

---
+++ oddjltest.jl
@@ -0,0 +1,28 @@
+using OpenDSSDirect
+
+systemName = "ads10_1ph" # this is something which the user will specify but will get saved into data
+
+filename = joinpath(@__DIR__, "rawData", systemName, "Master.dss")
+
+dss("""
+    clear
+    redirect "$filename"
+    solve
+""")
+
+function main()
+
+    loadnumber = Loads.First()
+    kWsum = 0.0
+    kvarsum = 0.0
+    
+    while loadnumber > 0
+        kWsum += Loads.kW()
+        kvarsum += Loads.kvar()
+        loadnumber = Loads.Next()
+    end
+
+    kWsum, kvarsum
+end
+
+main()
