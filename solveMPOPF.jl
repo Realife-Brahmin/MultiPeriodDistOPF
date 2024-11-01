51075be66053456c5420bab07290da3a811c551e
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-11-01 14:53:47 -0700

    opf for T=1 seems to work, so perhaps I can verify faster

--- solveMPOPF.jl
+++ solveMPOPF.jl
@@ -6,7 +6,7 @@ using Debugger
 
 Revise.revise()
 systemName = "ads10_1ph" # this is something which the user will specify but will get saved into data
-T = 24
+T = 1
 numAreas = 1
 temporal_decmp = false
 # objfun0 = "powerflow"
