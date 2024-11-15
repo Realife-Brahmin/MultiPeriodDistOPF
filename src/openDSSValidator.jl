835b63a4b21a793b3ed688987668aa69896ffee4
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-11-14 17:27:56 -0800

    rmunlocs

--- src/openDSSValidator.jl
+++ src/openDSSValidator.jl
@@ -22,89 +22,76 @@ using Parameters: @unpack
 include("helperFunctions.jl")
 using .helperFunctions: myprintln
 
-function validate_opf_against_opendss(model, data; filename="validation_results.csv")
-    # Set paths for DSS files
-    system_name = data[:systemName]
-    dss_dir = joinpath(dirname(@__DIR__), "rawData", system_name)
-    dss_file = joinpath(dss_dir, "Master.dss")
-    println(dss_file)
-    # Initialize OpenDSS
-    OpenDSSDirect.Text.Command("Clear")
-    # OpenDSSDirect.Text.Command("Redirect $dss_file")
-    OpenDSSDirect.Text.Command("Redirect '$dss_file'")
+# function validate_opf_against_opendss(model, data; filename="validation_results.csv")
+#     # Set paths for DSS files
+#     system_name = data[:systemName]
+#     dss_dir = joinpath(dirname(@__DIR__), "rawData", system_name)
+#     dss_file = joinpath(dss_dir, "Master.dss")
+#     println(dss_file)
+#     # Initialize OpenDSS
+#     OpenDSSDirect.Text.Command("Clear")
+#     # OpenDSSDirect.Text.Command("Redirect $dss_file")
+#     OpenDSSDirect.Text.Command("Redirect '$dss_file'")
 
 
-    # List all components in the circuit
-    component_names = OpenDSSDirect.Circuit.AllElementNames()
+#     # List all components in the circuit
+#     component_names = OpenDSSDirect.Circuit.AllElementNames()
 
-    # Display component names and types
-    for component in component_names
-        println("Component: $component")
-    end
+#     # Display component names and types
+#     for component in component_names
+#         println("Component: $component")
+#     end
 
-    # Unpack data
-    T = data[:T]
-    kVA_B = data[:kVA_B]
-    load_shape_pv = data[:LoadShapePV]
-    @unpack Dset, Bset = data  # PV and Battery bus sets
+#     # Unpack data
+#     T = data[:T]
+#     kVA_B = data[:kVA_B]
+#     load_shape_pv = data[:LoadShapePV]
+#     @unpack Dset, Bset = data  # PV and Battery bus sets
 
-    # Extract battery charge (P_c) and discharge (P_d) from the model
-    P_c = model[:P_c]
-    P_d = model[:P_d]
+#     # Extract battery charge (P_c) and discharge (P_d) from the model
+#     P_c = model[:P_c]
+#     P_d = model[:P_d]
 
-    # Initialize results DataFrame
-    results = DataFrame(
-        t=1:T,
-        PLoss_kW=zeros(T),
-        PSubs_kW=zeros(T),
-        QSubs_kVAr=zeros(T),
-        Voltages=Vector{Vector{Float64}}(undef, T)
-    )
+#     # Initialize results DataFrame
+#     results = DataFrame(
+#         t=1:T,
+#         PLoss_kW=zeros(T),
+#         PSubs_kW=zeros(T),
+#         QSubs_kVAr=zeros(T),
+#         Voltages=Vector{Vector{Float64}}(undef, T)
+#     )
 
-    for t in 1:T
-        # Set power levels for PVs based on current timestep
-        for pv_bus in Dset
-            OpenDSSDirect.Circuit.SetActiveElement("PVSystem.pv$(pv_bus)")
-            OpenDSSDirect.PVSystem.kW(pv_bus, load_shape_pv[t] * kVA_B)
-        end
+#     for t in 1:T
+#         # Set power levels for PVs based on current timestep
+#         for pv_bus in Dset
+#             OpenDSSDirect.Circuit.SetActiveElement("PVSystem.pv$(pv_bus)")
+#             OpenDSSDirect.PVSystem.kW(pv_bus, load_shape_pv[t] * kVA_B)
+#         end
 
-        # # Set battery power for each battery bus based on P_c and P_d values
-        # for battery_bus in Bset
-        #     charge_power_kW = value(P_d[battery_bus, t]) * kVA_B
-        #     discharge_power_kW = value(P_c[battery_bus, t]) * kVA_B
+#         # Solve the power flow
+#         OpenDSSDirect.Solution.Solve()
 
-        #     # Calculate net power for the battery (discharge - charge)
-        #     net_power_kW = charge_power_kW - discharge_power_kW
+#         # Retrieve circuit losses
+#         total_losses = OpenDSSDirect.Circuit.Losses() ./ 1000  # Convert W to kW
+#         results.PLoss_kW[t] = total_losses[1]
 
-        #     # Set battery power at this timestep
-        #     OpenDSSDirect.Circuit.SetActiveElement("Storage.$battery_bus")
-        #     OpenDSSDirect.Storage.kW(battery_bus, net_power_kW)
-        # end
+#         # Retrieve substation real and reactive power
+#         OpenDSSDirect.Circuit.SetActiveElement("Line.L1")
+#         substation_powers = OpenDSSDirect.CktElement.Powers()
+#         results.PSubs_kW[t] = sum(substation_powers[1:2:end])  # Summing real power across phases
+#         results.QSubs_kVAr[t] = sum(substation_powers[2:2:end])  # Summing reactive power across phases
 
-        # Solve the power flow
-        OpenDSSDirect.Solution.Solve()
+#         # Capture voltage magnitudes at all buses
+#         results.Voltages[t] = OpenDSSDirect.Circuit.AllBusVmagPu()
 
-        # Retrieve circuit losses
-        total_losses = OpenDSSDirect.Circuit.Losses() ./ 1000  # Convert W to kW
-        results.PLoss_kW[t] = total_losses[1]
+#         # Print the key results for this timestep
+#         println("Time: $t, PLoss: $(results.PLoss_kW[t]) kW, PSubs: $(results.PSubs_kW[t]) kW, QSubs: $(results.QSubs_kVAr[t]) kVAr")
+#     end
 
-        # Retrieve substation real and reactive power
-        OpenDSSDirect.Circuit.SetActiveElement("Line.L1")
-        substation_powers = OpenDSSDirect.CktElement.Powers()
-        results.PSubs_kW[t] = sum(substation_powers[1:2:end])  # Summing real power across phases
-        results.QSubs_kVAr[t] = sum(substation_powers[2:2:end])  # Summing reactive power across phases
-
-        # Capture voltage magnitudes at all buses
-        results.Voltages[t] = OpenDSSDirect.Circuit.AllBusVmagPu()
-
-        # Print the key results for this timestep
-        println("Time: $t, PLoss: $(results.PLoss_kW[t]) kW, PSubs: $(results.PSubs_kW[t]) kW, QSubs: $(results.QSubs_kVAr[t]) kVAr")
-    end
-
-    # Save the results
-    CSV.write(filename, results)
-    println("Validation results written to $filename")
-end
+#     # Save the results
+#     CSV.write(filename, results)
+#     println("Validation results written to $filename")
+# end
 
 function compute_highest_allTime_voltage_discrepancy(model, data, vald)
     # Initialize the maximum voltage discrepancy
@@ -135,33 +122,6 @@ function compute_highest_allTime_voltage_discrepancy(model, data, vald)
 
     return disc_voltage_all_time_pu
 end
-
-# Todo: This function should be in exporter instead of this mod
-# function export_validation_decision_variables(vald, data; verbose::Bool=false)
-
-#     # Define the path and filename based on the specified structure
-#     @unpack T, systemName, numAreas, gedAppendix, machine_ID, objfunConciseDescription, processedDataFolderPath, simNatureAppendix, solver = data
-#     base_dir = joinpath(processedDataFolderPath, systemName, gedAppendix, "Horizon_$(T)", "numAreas_$(numAreas)")
-
-#     # Create the directory if it doesn't exist
-#     if !isdir(base_dir)
-#         if verbose
-#             println("Creating directory: $base_dir")
-#         end
-#         mkpath(base_dir)
-#     end
-
-#     # Define the filename with the appropriate structure
-#     filename = joinpath(base_dir, "Horizon_$(T)_$(machine_ID)_$(solver)_validationDecisionVariables_$(gedAppendix)_for_$(objfunConciseDescription)_via_$(simNatureAppendix).txt")
-
-#     # Write the vald dictionary to a CSV file
-#     CSV.write(filename, vald)
-
-#     # Print confirmation if verbose is enabled
-#     if verbose
-#         println("Validation decision variables written to $filename")
-#     end
-# end
 
 function get_battery_powers_opendss_powerflow_for_timestep_t(;
     verbose::Bool=false)
