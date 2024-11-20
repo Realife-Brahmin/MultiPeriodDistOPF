0ba4256c48cf45996e3a509302c15928152b2d23
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-11-20 03:43:16 -0800

    m

--- src/playbook_of_mpopf.jl
+++ src/playbook_of_mpopf.jl
@@ -107,7 +107,6 @@ function build_MPOPF_1ph_NL_model_t_1toT(data)
         l_ij_t = l[(i, j), t]
         line_reactive_loss = x_ij * l_ij_t
 
-        # Todo: Figure out whether p_L_pu[i][j] be allowed to exist or should it be made the same as p_L_pu[i, j].
         # Reactive load at node j and time t
         q_L_j_t = (j in NLset) ? q_L_pu[(j, t)] : 0.0  # Assign 0.0 if j is not in Nset
 
