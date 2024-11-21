821c8765d2efeb17d69c955df5c573b6922db7bd
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-11-21 07:51:15 -0800

    Refactor Hyperparameters module and crate  objective function to use estimated alpha value. Currently only a skeleton function

--- src/ModelBuilder/Hyperparameters.jl
+++ src/ModelBuilder/Hyperparameters.jl
@@ -1,10 +1,20 @@
 module Hyperparameters
 
-export estimate_alpha, estimate_gamma
+export estimate_alpha
 
 using Parameters: @unpack
+include("../functionRetriever.jl")
+import .functionRetriever as FR
 
-function estimate_alpha(model, data; Tset=nothing)
+function estimate_alpha(data)
+    @unpack LoadShapeCost, kVA_B = data;
+    C = LoadShapeCost # dollars_per_kWh
+    dollars_per_kWh = C
+    dollars_per_pu = C * kVA_B
+    load_real_power_vs_t_1toT_kW = FR.get_load_real_power(data, horizon="1toT")
+    line_loss_accommodation_factor = 1.1
+    fcost_est = dollars_per_pu .* load_real_power_vs_t_1toT_kW * line_loss_accommodation_factor
+    println("fcost_est = $fcost_est")
     alpha = 1e-3
     return alpha
 end
--- src/ModelBuilder/Objective.jl
+++ src/ModelBuilder/Objective.jl
@@ -5,17 +5,20 @@ export define_objective_function_t_in_Tset
 using JuMP
 using Parameters: @unpack
 
+include("Hyperparameters.jl")
+import .Hyperparameters as HP
+
 function define_objective_function_t_in_Tset(model, data; Tset=nothing, tSOC_hard=false)
     if Tset === nothing
         Tset = data[:Tset]
     end
 
-    @unpack objfun0, objfun2, Bset, kVA_B, rdict_pu, Lset, alpha, gamma, Bref_pu = data
-    P_Subs = model[:P_Subs]
-    P_c = model[:P_c]
-    P_d = model[:P_d]
-    l = model[:l]
-    B = model[:B]
+    @unpack objfun0, objfun2 = data
+    # P_Subs = model[:P_Subs]
+    # P_c = model[:P_c]
+    # P_d = model[:P_d]
+    # l = model[:l]
+    # B = model[:B]
 
     # Assume objfun0 and objfun2 are passed to the function that defines the model.
     if objfun0 == "powerflow"
@@ -23,14 +26,17 @@ function define_objective_function_t_in_Tset(model, data; Tset=nothing, tSOC_har
         objfun = 0
     elseif objfun0 == "subsPowerCostMin"
         # Define the base objective function (generation cost minimization)
-        @unpack LoadShapeCost, delta_t = data
+        @unpack LoadShapeCost, delta_t, kVA_B = data
         C = LoadShapeCost
         dollars_per_pu = kVA_B
+        P_Subs = model[:P_Subs]
         objfun = sum(
             dollars_per_pu * C[t] * P_Subs[t] * delta_t
             for t in Tset
         )
     elseif objfun0 == "lineLossMin"
+        l = model[:l]
+        @unpack Lset, rdict_pu = data;
         objfun = sum(
             rdict_pu[(i, j)] * l[(i, j), t]
             for (i, j) in Lset, t in Tset
@@ -39,20 +45,26 @@ function define_objective_function_t_in_Tset(model, data; Tset=nothing, tSOC_har
 
     # Append the alpha term only if objfun2 == "scd"
     if objfun2 == "scd"
-        @unpack eta_C, eta_D = data
+        @unpack Bset, eta_C, eta_D = data;
         η_C = eta_C
         η_D = eta_D
+        alpha = HP.estimate_alpha(data)
+        α = alpha
+        P_c = model[:P_c]
+        P_d = model[:P_d]
         objfun += sum(
-            alpha * ((1 - η_C[j]) * P_c[j, t] + (1 / η_D[j] - 1) * P_d[j, t])
+            α * ((1 - η_C[j]) * P_c[j, t] + (1 / η_D[j] - 1) * P_d[j, t])
             for j in Bset, t in Tset
         )
     end
 
     # Append the gamma term only if tSOC_hard is false
     if !tSOC_hard
-        @unpack T = data
+        @unpack T, Bset, gamma, Bref_pu = data;
+        B = model[:B]
+        γ = gamma
         objfun += sum(
-            gamma * (B[j, T] - Bref_pu[j])^2
+            γ * (B[j, T] - Bref_pu[j])^2
             for j in Bset
         )
     end
