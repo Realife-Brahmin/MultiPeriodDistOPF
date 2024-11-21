d8681582d117a5cea66e28e2236ec2d5641a49a1
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-11-21 10:16:16 -0800

    fcost now sensibly estimated for estimation of my alpha

--- src/ModelBuilder/Hyperparameters.jl
+++ src/ModelBuilder/Hyperparameters.jl
@@ -12,8 +12,8 @@ function estimate_alpha(data)
     dollars_per_kWh = C
     load_real_power_vs_t_1toT_kW = FR.get_load_real_power(data, horizon="1toT")
     line_loss_accommodation_factor = 1.1
-    fcost_est = dollars_per_kWh .* load_real_power_vs_t_1toT_kW * line_loss_accommodation_factor
-    println("fcost_est = $fcost_est")
+    fcost_est = line_loss_accommodation_factor * transpose(dollars_per_kWh) * load_real_power_vs_t_1toT_kW
+    # println("fcost_est = $fcost_est")
     alpha = 1e-3
     return alpha
 end
