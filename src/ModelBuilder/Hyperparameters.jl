d336376cee0ab3a3e925eac12e5c0badb904fbd9
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-11-21 10:09:54 -0800

    Fix cost estimation calculation in alpha estimation function

--- src/ModelBuilder/Hyperparameters.jl
+++ src/ModelBuilder/Hyperparameters.jl
@@ -10,10 +10,9 @@ function estimate_alpha(data)
     @unpack LoadShapeCost, kVA_B = data;
     C = LoadShapeCost # dollars_per_kWh
     dollars_per_kWh = C
-    dollars_per_pu = C * kVA_B
     load_real_power_vs_t_1toT_kW = FR.get_load_real_power(data, horizon="1toT")
     line_loss_accommodation_factor = 1.1
-    fcost_est = dollars_per_pu .* load_real_power_vs_t_1toT_kW * line_loss_accommodation_factor
+    fcost_est = dollars_per_kWh .* load_real_power_vs_t_1toT_kW * line_loss_accommodation_factor
     println("fcost_est = $fcost_est")
     alpha = 1e-3
     return alpha
