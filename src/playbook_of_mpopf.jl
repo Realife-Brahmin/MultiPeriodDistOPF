5096b8e80bfcdf8508d048f919c482b48605cf61
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-11-20 21:07:47 -0800

    refactor variable initialization into a separate function

--- src/playbook_of_mpopf.jl
+++ src/playbook_of_mpopf.jl
@@ -573,6 +573,63 @@ function define_objective_function_t_in_Tset(model, data; Tset=nothing, tSOC_har
     return model
 end
 
+function initialize_variables_t_in_Tset(model, data; Tset=nothing)
+    if Tset === nothing
+        Tset = data[:Tset]
+    end
+
+    # Initialize substation power flow variable
+    P_Subs = model[:P_Subs]
+    for t in Tset
+        set_start_value(P_Subs[t], 0.0)
+    end
+
+    # Initialize power flow variables
+    @unpack Lset = data
+    P = model[:P]
+    Q = model[:Q]
+    l = model[:l]
+    for (i, j) in Lset, t in Tset
+        set_start_value(P[(i, j), t], 0.0)
+        set_start_value(Q[(i, j), t], 0.0)
+        set_start_value(l[(i, j), t], 0.0)
+    end
+
+    # Initialize voltage variables
+    @unpack Compset, V_Subs = data
+    v = model[:v]
+    for j in Compset, t in Tset
+        set_start_value(v[j, t], (V_Subs)^2)
+    end
+
+    # Initialize PV inverter reactive dispatch variables
+    @unpack Dset = data
+    q_D = model[:q_D]
+    for j in Dset, t in Tset
+        set_start_value(q_D[j, t], 0.0)
+    end
+
+    # Initialize battery real and reactive dispatch variables
+    @unpack Bset = data
+    q_B = model[:q_B]
+    P_c = model[:P_c]
+    P_d = model[:P_d]
+    for j in Bset, t in Tset
+        set_start_value(q_B[j, t], 0.0)
+        set_start_value(P_c[j, t], 0.0)
+        set_start_value(P_d[j, t], 0.0)
+    end
+
+    # Initialize battery state of charge (SOC) variables
+    @unpack B0_pu = data
+    B = model[:B]
+    for j in Bset, t in Tset
+        set_start_value(B[j, t], B0_pu[j])
+    end
+
+    return model
+end
+
 function build_MPOPF_1ph_NL_model_t_1toT(data)
     @unpack solver = data
 
@@ -658,46 +715,47 @@ function build_MPOPF_1ph_NL_model_t_1toT(data)
     # ===========================
     # Initializing Variables
     # ===========================
+    model = initialize_variables_t_in_Tset(model, data)
 
-    @unpack Tset = data
-    # Initialize substation power flow variable
-    for t in Tset
-        set_start_value(P_Subs[t], 0.0)
-    end
+    # @unpack Tset = data
+    # # Initialize substation power flow variable
+    # for t in Tset
+    #     set_start_value(P_Subs[t], 0.0)
+    # end
 
-    @unpack Tset, Lset = data
-    # Initialize power flow variables
-    for (i, j) in Lset, t in Tset
-        set_start_value(P[(i, j), t], 0.0)
-        set_start_value(Q[(i, j), t], 0.0)
-        set_start_value(l[(i, j), t], 0.0)
-    end
+    # @unpack Tset, Lset = data
+    # # Initialize power flow variables
+    # for (i, j) in Lset, t in Tset
+    #     set_start_value(P[(i, j), t], 0.0)
+    #     set_start_value(Q[(i, j), t], 0.0)
+    #     set_start_value(l[(i, j), t], 0.0)
+    # end
 
-    @unpack Tset, Compset, V_Subs = data
-    # Initialize voltage variables
-    for j in Compset, t in Tset
-        set_start_value(v[j, t], (V_Subs)^2)
-    end
+    # @unpack Tset, Compset, V_Subs = data
+    # # Initialize voltage variables
+    # for j in Compset, t in Tset
+    #     set_start_value(v[j, t], (V_Subs)^2)
+    # end
 
-    @unpack Tset, Dset = data
-    # Initialize PV inverter reactive dispatch variables
-    for j in Dset, t in Tset
-        set_start_value(q_D[j, t], 0.0)
-    end
+    # @unpack Tset, Dset = data
+    # # Initialize PV inverter reactive dispatch variables
+    # for j in Dset, t in Tset
+    #     set_start_value(q_D[j, t], 0.0)
+    # end
 
-    @unpack Tset, Bset = data
-    # Initialize battery real and reactive dispatch variables
-    for j in Bset, t in Tset
-        set_start_value(q_B[j, t], 0.0)
-        set_start_value(P_c[j, t], 0.0)
-        set_start_value(P_d[j, t], 0.0)
-    end
+    # @unpack Tset, Bset = data
+    # # Initialize battery real and reactive dispatch variables
+    # for j in Bset, t in Tset
+    #     set_start_value(q_B[j, t], 0.0)
+    #     set_start_value(P_c[j, t], 0.0)
+    #     set_start_value(P_d[j, t], 0.0)
+    # end
 
-    @unpack Tset, Bset, B0_pu = data
-    # Initialize battery state of charge (SOC) variables
-    for j in Bset, t in Tset
-        set_start_value(B[j, t], B0_pu[j])
-    end
+    # @unpack Tset, Bset, B0_pu = data
+    # # Initialize battery state of charge (SOC) variables
+    # for j in Bset, t in Tset
+    #     set_start_value(B[j, t], B0_pu[j])
+    # end
 
     return model
 end
