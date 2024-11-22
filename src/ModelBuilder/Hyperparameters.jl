bc68d4c01258ec03a5dc247b1de94eed7ac770df
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-11-22 07:47:10 -0800

    Add line losses estimation function and clean up alpha calculation

--- src/ModelBuilder/Hyperparameters.jl
+++ src/ModelBuilder/Hyperparameters.jl
@@ -13,8 +13,6 @@ function estimate_alpha(data)
     else
         fobj_est = 0
     end
-    # fobj_est = estimate_substation_power_cost(data)
-    # alpha = 1e-3
     fscd_est = estimate_fscd(data)
     alpha = fobj_est / fscd_est
     return alpha
@@ -36,6 +34,15 @@ function estimate_substation_power_cost(data)
     fcost_est = line_loss_accommodation_factor * transpose(dollars_per_kWh) * load_real_power_vs_t_1toT_kW
     
     return fcost_est
+end
+
+function estimate_line_losses(data)
+    @unpack kVA_B = data
+    load_real_power_allT_kW = FR.get_load_real_power(data, horizon="allT")
+    line_loss_accommodation_factor = 0.1 # 10% line loss accommodation factor, just a usual value
+    fPLoss_est = line_loss_accommodation_factor *  load_real_power_vs_t_1toT_kW
+
+    return fPLoss_est
 end
 
 end # Hyperparameters module
