f232c1e4e14863a5031ce5592514a2bd7df4e861
= <aryan.r.jha@gmail.com>
2024-12-26 04:51:26 -0800

    added locs for testing B values across k

--- testing_ddp_iteration_variable_updates.jl
+++ testing_ddp_iteration_variable_updates.jl
@@ -17,9 +17,13 @@ t_ddp = rand(1:T)
 println("t_ddp = $t_ddp")
 for j in Bset
     values_mu = []
+    values_B = []
     for k_ddp in 1:3
-        push!(values_mu, trim_number_for_printing(μ[j, t_ddp, k_ddp], sigdigits=2))
+        B = modelVals_ddp_vs_t_vs_k[t_ddp, k_ddp][:B]
+        push!(values_B, trim_number_for_printing(B[j, t_ddp], sigdigits=2))
+        # push!(values_mu, trim_number_for_printing(μ[j, t_ddp, k_ddp], sigdigits=2))
     end
-    println("j = $j: μ = ", join(values_mu, ", "))
+    # println("j = $j: μ = ", join(values_mu, ", "))
+    println("j = $j: B = ", join(values_B, ", "))
 
 end
