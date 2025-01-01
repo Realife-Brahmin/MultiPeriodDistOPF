d042d68114ecf9ebb0242084638476ec13c8c9d2
= <aryan.r.jha@gmail.com>
2025-01-01 09:10:10 -0800

    trying to hunt the numbers

--- main.jl
+++ main.jl
@@ -6,12 +6,12 @@ using Parameters
 Revise.revise()
 
 systemName = "ads10_1ph"
-systemName = "ieee123_1ph"
+# systemName = "ieee123_1ph"
 T0 = 24
 # T0 = 3
-# factor = 1/8
-factor = 1
-factor = 20
+factor = 1/8
+# factor = 1
+# factor = 20
 T = Int(T0*factor) 
 numAreas = 1
 temporal_decmp = false
