992edebcade58e2717739c4035a9bb5489b080ed
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2025-01-20 21:52:38 -0800

    faroundandfindout

--- src/DDP/DDP.jl
+++ src/DDP/DDP.jl
@@ -102,7 +102,8 @@ function build_ForwardStep_1ph_NL_model_t_is_1(ddpModel;
     objfun_expr_t0_without_mu_terms = objective_function(model_t0) 
     μ = mu
     @unpack Bset = data;
-    objfun_expr_t0_k_with_mu_terms = objfun_expr_t0_without_mu_terms + sum(μ[j, t_ddp+1, k_ddp-1] * (-model_t0[:B][j, t_ddp]) for j ∈ Bset)
+    # objfun_expr_t0_k_with_mu_terms = objfun_expr_t0_without_mu_terms + sum(μ[j, t_ddp+1, k_ddp-1] * (-model_t0[:B][j, t_ddp]) for j ∈ Bset)
+    objfun_expr_t0_k_with_mu_terms = objfun_expr_t0_without_mu_terms - sum(μ[j, t_ddp+1, k_ddp-1] * (-model_t0[:B][j, t_ddp]) for j ∈ Bset)
 
     @objective(model_t0, Min, objfun_expr_t0_k_with_mu_terms)
 
