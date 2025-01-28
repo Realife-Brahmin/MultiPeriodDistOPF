d9ca04f4e7e0ea88618425fb4613c081656211d6
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2025-01-27 22:39:33 -0800

    refactor print_mu function to handle ddpModel and enhance output for dual variables and KKT conditions

--- src/playbook_of_mpopf.jl
+++ src/playbook_of_mpopf.jl
@@ -100,7 +100,59 @@ function get_soc_dual_variables_fullMPOPF(modelDict; Tset=nothing)
     return mu
 end
 
-function print_mu(modelDict)
+# function print_mu(modelDict)
+#     crayon_header = Crayon(foreground=:white, background=:blue, bold=true)
+#     crayon_error = Crayon(foreground=:red, bold=true)
+
+#     # Define colors for each battery for non-temporal decomposition
+#     battery_colors_non_temporal = Dict(
+#         1 => Crayon(foreground=:red, bold=true),
+#         2 => Crayon(foreground=:green, bold=true),
+#         3 => Crayon(foreground=:blue, bold=true)
+#     )
+
+#     # Define negative colors for each battery for temporal decomposition
+#     battery_colors_temporal = Dict(
+#         1 => Crayon(foreground=:light_red, bold=true),
+#         2 => Crayon(foreground=:light_green, bold=true),
+#         3 => Crayon(foreground=:light_blue, bold=true)
+#     )
+
+#     println(crayon_header("Dual Variables (mu) for SOC Constraints:"))
+
+#     @unpack mu, data = modelDict
+#     @unpack Tset, Bset, temporal_decmp = data
+
+#     # Limit to the first 2 batteries if there are more than 2
+#     Bset_to_print = length(Bset) > 2 ? [Bset[1], Bset[end]] : Bset
+
+#     if temporal_decmp == false
+#         for (j_B, j) in enumerate(Bset_to_print)
+#             battery_color = battery_colors_non_temporal[j_B]
+#             for t in Tset
+#                 if haskey(mu, (j, t))
+#                     println(battery_color("mu[$j, $t] = $(mu[(j, t)])"))
+#                 else
+#                     println(crayon_error("mu[$j, $t] not found"))
+#                 end
+#             end
+#         end
+#     elseif temporal_decmp == true
+#         @unpack k_ddp = modelDict
+#         for (j_B, j) in enumerate(Bset_to_print)
+#             battery_color = battery_colors_temporal[j_B]
+#             for t in Tset
+#                 if haskey(mu, (j, t, k_ddp - 1))
+#                     println(battery_color("mu[$j, $t, $(k_ddp-1)] = $(mu[(j, t, k_ddp-1)])"))
+#                 else
+#                     println(crayon_error("mu[$j, $t, $(k_ddp-1)] not found"))
+#                 end
+#             end
+#         end
+#     end
+# end
+
+function print_mu(ddpModel)
     crayon_header = Crayon(foreground=:white, background=:blue, bold=true)
     crayon_error = Crayon(foreground=:red, bold=true)
 
@@ -120,13 +172,14 @@ function print_mu(modelDict)
 
     println(crayon_header("Dual Variables (mu) for SOC Constraints:"))
 
-    @unpack mu, data = modelDict
+    @unpack mu, data, k_ddp = ddpModel
     @unpack Tset, Bset, temporal_decmp = data
 
-    # Limit to the first 2 batteries if there are more than 2
+    # Limit to the first and last batteries if there are more than 2
     Bset_to_print = length(Bset) > 2 ? [Bset[1], Bset[end]] : Bset
 
     if temporal_decmp == false
+        @unpack model = ddpModel
         for (j_B, j) in enumerate(Bset_to_print)
             battery_color = battery_colors_non_temporal[j_B]
             for t in Tset
@@ -138,7 +191,7 @@ function print_mu(modelDict)
             end
         end
     elseif temporal_decmp == true
-        @unpack k_ddp = modelDict
+        @unpack models_ddp_vs_t_vs_k = ddpModel
         for (j_B, j) in enumerate(Bset_to_print)
             battery_color = battery_colors_temporal[j_B]
             for t in Tset
@@ -150,6 +203,58 @@ function print_mu(modelDict)
             end
         end
     end
+
+    println(crayon_header("Dual Variables (lambda) for SOC Limits:"))
+
+    for (j_B, j) in enumerate(Bset_to_print)
+        battery_color = temporal_decmp ? battery_colors_temporal[j_B] : battery_colors_non_temporal[j_B]
+        for t in Tset
+            if temporal_decmp == false
+                @unpack model = ddpModel
+            else
+                model = models_ddp_vs_t_vs_k[t, k_ddp-1]
+            end
+            lambda_lower_name = "g_11_j^t_MinSOC_Node_j_$(j)_t_$(t)"
+            lambda_upper_name = "g_12_j^t_MaxSOC_Node_j_$(j)_t_$(t)"
+            lambda_lower = dual(constraint_by_name(model, lambda_lower_name))
+            lambda_upper = dual(constraint_by_name(model, lambda_upper_name))
+            println(battery_color("lambda_lower[$j, $t] = $lambda_lower"))
+            println(battery_color("lambda_upper[$j, $t] = $lambda_upper"))
+        end
+    end
+
+    println(crayon_header("Checking for KKT Necessary Condition (∇L_{B_j^t} = 0 ∀ j ∈ B, t ∈ τ):"))
+
+    for (j_B, j) in enumerate(Bset_to_print)
+        battery_color = temporal_decmp ? battery_colors_temporal[j_B] : battery_colors_non_temporal[j_B]
+        for t in Tset
+            if t < maximum(Tset)
+                if temporal_decmp == false
+                    @unpack model = ddpModel
+                else
+                    model = models_ddp_vs_t_vs_k[t, k_ddp-1]
+                end
+                lambda_lower_name = "g_11_j^t_MinSOC_Node_j_$(j)_t_$(t)"
+                lambda_upper_name = "g_12_j^t_MaxSOC_Node_j_$(j)_t_$(t)"
+                lambda_lower = dual(constraint_by_name(model, lambda_lower_name))
+                lambda_upper = dual(constraint_by_name(model, lambda_upper_name))
+                if temporal_decmp == false
+                    mu_current = mu[(j, t)]
+                    mu_next = mu[(j, t + 1)]
+                else
+                    mu_current = mu[(j, t, k_ddp - 1)]
+                    mu_next = mu[(j, t + 1, k_ddp - 1)]
+                end
+
+                balance = -lambda_lower + lambda_upper + mu_current - mu_next
+                println(battery_color("∇L_{B_j^t} for [$j, $t]: $balance"))
+            end
+        end
+    end
 end
+
+# Example usage:
+# Assuming ddpModel contains the mu values and data contains Tset and Bset
+# print_mu(ddpModel)
 
 end # module Playbook_of_MPOPF
