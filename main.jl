bddc7c5bae8e1f2b010762e76c0c7a2e7e2cea8d
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2025-01-30 15:59:46 -0800

    m

--- main.jl
+++ main.jl
@@ -6,16 +6,16 @@ using Parameters
 Revise.revise()
 
 systemName = "ads10_1ph"
-# systemName = "ieee123_1ph"
-T0 = 3
-# T0 = 24
+systemName = "ieee123_1ph"
+# T0 = 3
+T0 = 24
 # T0 = 11
 # factor = 1/2
 factor = 1
 T = Int(T0*factor) 
 numAreas = 1
 temporal_decmp = false
-temporal_decmp = true
+# temporal_decmp = true
 savePlots = false
 savePlots = true
 # objfun0 = "powerflow"
