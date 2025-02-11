abc179282b2efca643924f1a1a1f55832546bc24
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2025-02-11 09:15:28 -0800

    trying to see how long this may work

--- src/playbook_of_mpopf.jl
+++ src/playbook_of_mpopf.jl
@@ -4,6 +4,7 @@ module Playbook_of_MPOPF
 export
     get_soc_dual_variables_fullMPOPF,
     optimize_MPOPF_1ph_NL_TemporallyBruteforced,
+    optimize_MPOPF_1ph_L,
     print_mu
 
 include("./ModelBuilder/ModelBuilder.jl")
@@ -71,6 +72,48 @@ function optimize_MPOPF_1ph_NL_TemporallyBruteforced(data)
     
     # Exporter.export_optimization_model(modelDict, verbose=false)
     
+    return modelDict
+
+end
+
+function optimize_MPOPF_1ph_L(data)
+
+    Tset = data[:Tset] # In this case, Tset = [1, 2, ... T]
+    modelDict = MB.build_MPOPF_1ph_L_model_t_in_Tset(data, Tset=Tset)
+
+    @unpack model, data = modelDict
+    optimize!(model)
+
+    @pack! modelDict = model
+
+    # modelDict = generate_1ph_L_model_decvar_value_dict(modelDict)
+    modelDict = MC.copy_modelVals(modelDict, model, Tset=Tset)
+
+    # Extract SOC dual variables and update modelDict
+    mu = get_soc_dual_variables_fullMPOPF(modelDict, Tset=Tset)
+    modelDict[:mu] = mu
+
+    # Check solver status and retrieve results
+    # Define crayons for green and red text
+    crayon_light_green = Crayon(foreground=:light_green, bold=true)
+    crayon_red = Crayon(foreground=:red, bold=true)
+
+    @unpack modelVals = modelDict
+    termination_status = modelVals[:termination_status]
+
+    crayon_final_green = Crayon(foreground=:white, background=:green, bold=true)
+    if termination_status == LOCALLY_SOLVED
+        println(crayon_final_green("Optimal solution found."))
+    else
+        println(crayon_red("Optimization did not find an optimal solution."))
+    end
+
+    # optimal_obj_value = objective_value(model)
+    optimal_obj_value = modelVals[:objective_value]
+    println("Optimal objective function value: ", optimal_obj_value)
+
+    # Exporter.export_optimization_model(modelDict, verbose=false)
+
     return modelDict
 
 end
