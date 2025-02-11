15233b7ae198b270875b4ff5f13f20d70af3ba5d
= <aryan.r.jha@gmail.com>
2025-02-11 06:25:04 -0800

    Remove deprecated reactive power limits function for battery inverters

--- src/ModelBuilder/ModelBuilder.jl
+++ src/ModelBuilder/ModelBuilder.jl
@@ -17,7 +17,6 @@ export
     nodalRealPowerBalance_non_substation_1ph_NL_t_in_Tset,
     nodalRealPowerBalance_substation_t_in_Tset,
     reactive_power_limits_battery_inverters_1ph_NL_t_in_Tset,
-    reactive_power_limits_battery_inverters_t_in_Tset,
     reactive_power_limits_PV_inverters_t_in_Tset,
     SOC_limits_batteries_t_in_Tset,
     voltage_limits_constraints_t_in_Tset
@@ -138,9 +137,6 @@ function build_MPOPF_1ph_NL_model_t_in_Tset(data;
 
     # Reactive power limits for PV inverters
     modelDict = reactive_power_limits_PV_inverters_t_in_Tset(modelDict, Tset=Tset)
-
-    # # Reactive power limits for battery inverters
-    # modelDict = reactive_power_limits_battery_inverters_t_in_Tset(modelDict, Tset=Tset)
 
     # Reactive power limits for battery inverters
     modelDict = reactive_power_limits_battery_inverters_1ph_NL_t_in_Tset(modelDict, Tset=Tset)
