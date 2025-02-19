c4112a213a2ddb6d3bb81d4684d887a668fa7fee
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2025-02-19 10:59:07 -0800

    Looks like Storage.dss creation script is working as intended, but will verify later

--- src/Parser/convertODDFiles.jl
+++ src/Parser/convertODDFiles.jl
@@ -41,3 +41,43 @@ end
 input_file_path = "C:/Users/aryan/Documents/documents_general/MultiPeriodDistOPF/rawData/OpenDSS730node/Loads.dss"
 output_file_path = "C:/Users/aryan/Documents/documents_general/MultiPeriodDistOPF/rawData/ieee730_1ph/Loads.dss"
 convert_files_loads_dss(input_file_path, output_file_path)
+function generate_battery_dss_from_loads_dss(loads_file_path::String; Batt_percent=30.0, Batt_rating_factor=1.0)
+    # Read the Loads.dss file
+    lines = readlines(loads_file_path)
+
+    # Extract non-zero loads
+    load_buses = []
+    for line in lines
+        if startswith(line, "New load.")
+            parts = split(line)
+            bus = split(parts[3], "=")[2]
+            kV = parse(Float64, split(parts[5], "=")[2])
+            kW = parse(Float64, split(parts[6], "=")[2])
+            if kW > 0
+                push!(load_buses, (bus, kV, kW))
+            end
+        end
+    end
+
+    # Calculate the number of batteries
+    @show N_L = length(load_buses)
+    @show num_batteries = ceil(Int, Batt_percent / 100 * N_L)
+    actual_Batt_percent = ceil(Int, num_batteries / N_L * 100)
+
+    # Select evenly spaced buses for battery placement
+    selected_buses = [load_buses[i] for i in 1:div(N_L, num_batteries):N_L][1:num_batteries]
+
+    # Generate the Storage<BattPercent>.dss file
+    storage_file_path = joinpath(dirname(loads_file_path), "Storage$(actual_Batt_percent).dss")
+    open(storage_file_path, "w") do file
+        for (i, (bus, kV, kW)) in enumerate(selected_buses)
+            kVA = Batt_rating_factor * kW
+            kWrated = kVA  # 100% of the load
+            kWhrated = 4 * kVA
+            println(file, "New Storage.Battery$(i+1) phases=1 Bus1=$(bus) kV=$(kV) kVA=$(round(kVA, digits=4)) kWrated=$(round(kWrated, digits=4)) kWhrated=$(round(kWhrated, digits=4)) %stored=62.5 %reserve=30 %EffCharge=95 %EffDischarge=95 %idlingkW=0 Vminpu=0.95 Vmaxpu=1.05 DispMode=External")
+        end
+    end
+
+    println("Generated $storage_file_path with $num_batteries batteries.")
+end
+
