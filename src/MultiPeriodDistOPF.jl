136645cd69603910dd9ab098b1a587214c9a4ed0
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2025-03-06 18:26:34 -0800

    From now a setup file will take care of acronymization of my own submodules (which I can then easily invoke in REPL). Though nothing I did was able to help me with Ctrl+Clicking on the called functions.

--- src/MultiPeriodDistOPF.jl
+++ src/MultiPeriodDistOPF.jl
@@ -3,72 +3,49 @@ module MultiPeriodDistOPF
 using JuMP
 using Parameters: @unpack, @pack!
 
+# Include and use submodules
 include("./computeOutputs.jl")
-using .computeOutputs
-
 include("./DDP/DDP.jl")
-using .DDP
-
 include("./functionRetriever.jl")
-using .functionRetriever
-
 include("./helperFunctions.jl")
-using .helperFunctions
-
 include("./ModelBuilder/ModelBuilder.jl")
-using .ModelBuilder
-
 include("./playbook_of_mpopf.jl")
-using .Playbook_of_MPOPF
-
 include("./Parser/parseOpenDSSFiles.jl")
-using .parseOpenDSSFiles
-
 include("./Plotter/Plotter.jl")
-using .Plotter
-
 include("./exporter.jl")
-using .Exporter
-
 include("./openDSSValidator.jl")
+
+# Use your own submodules
+using .computeOutputs
+using .DDP
+using .functionRetriever
+using .helperFunctions
+using .ModelBuilder
+using .Playbook_of_MPOPF
+using .parseOpenDSSFiles
+using .Plotter
+using .Exporter
 using .openDSSValidator
 
-export @unpack,
-    @pack!,
-    attach_solver,
+# Explicitly import and re-export macros from external packages (correct way)
+export @unpack, @pack!
+
+# Explicitly export your submodule functions you want accessible externally
+export optimize_MPOPF_1ph_NL_TemporallyBruteforced,
+    optimize_MPOPF_1ph_L,
+    optimize_MPOPF_1ph_NL_DDP,
+    parse_all_data,
     compute_output_values,
-    configure_solver,
-    evaluate_voltage_limits,
     export_decision_variables,
-    export_optimization_model,
     export_simulation_key_results_txt,
     export_validation_decision_variables,
     export_validation_key_results,
-    generateBinaryLoadShape,
-    get_load_real_power,
-    get_scd,
-    get_soc_dual_variables_fullMPOPF,
-    get_source_bus,
-    get_substation_lines,
-    myprintln,
-    optimize_MPOPF_1ph_NL_DDP,
-    optimize_MPOPF_1ph_NL_TemporallyBruteforced,
-    optimize_MPOPF_1ph_L,
-    parse_all_data,
-    parse_battery_data,
-    parse_branch_data,
-    parse_load_data,
-    parse_pv_data,
-    parse_system_simulation_data,
     plot_battery_actions,
     plot_input_forecast_curves,
     plot_line_losses,
     plot_substation_power,
     plot_substation_power_cost,
     plot_substation_power_cost_allT_vs_k,
-    print_mu,
-    set_custom_load_shape!,
-    trim_number_for_printing,
     validate_opf_against_opendss
 
-end # module
+end # module MultiPeriodDistOPF
