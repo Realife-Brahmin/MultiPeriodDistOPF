0f7b1116e4b24ebad780f50b7c280690f29a1372
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2025-03-06 18:27:02 -0800

    let's call it a day

--- main.jl
+++ main.jl
@@ -1,21 +1,19 @@
-# solveMPOPF.jl
-using Revise
-using MultiPeriodDistOPF
-using Parameters
-
-Revise.revise()
+# main.jl
+include("./src/setupMultiPeriodDistOPF.jl") 
 
 begin
     # systemName0 = "ads10_1ph"
     # systemName0 = "ieee123_1ph-A"
     # systemName0 = "ieee123_1ph-B"
-    systemName0 = "ieee729_1ph"
-    # systemName0 = "ieee730_1ph"
-    # T0 = 3
+    # systemName0 = "ieee729_1ph"
+    systemName0 = "ieee730_1ph"
+    T0 = 3
     # T0 = 24
-    T0 = 11
+    # T0 = 11
     factor = 1
     # factor = 1/2
+    linearizedModel = false
+    linearizedModel = true
     temporal_decmp = false
     # temporal_decmp = true
 end;
@@ -23,8 +21,6 @@ end;
 begin
     T = Int(T0*factor) 
     numAreas = 1
-    linearizedModel = false
-    # linearizedModel = true
     maxiter_ddp = 8
     savePlots = false
     savePlots = true
@@ -77,25 +73,25 @@ end;
 
 begin
     # Parse all data
-    data = parse_all_data(systemName, T, temporal_decmp=temporal_decmp, linearizedModel=linearizedModel, relax_terminal_soc_constraint=relax_terminal_soc_constraint, gedDict_ud=gedDict_ud);
+    data = Parser.parse_all_data(systemName, T, temporal_decmp=temporal_decmp, linearizedModel=linearizedModel, relax_terminal_soc_constraint=relax_terminal_soc_constraint, gedDict_ud=gedDict_ud)
 
     @unpack kVA_B_dict, MVA_B_dict, kV_B_dict, rdict, xdict, rdict_pu, xdict_pu, Z_B_dict, Lset, Nset, NLset = data;
 end;
 
+#region Optimization
 if !temporal_decmp
     if !linearizedModel
-        modelDict = optimize_MPOPF_1ph_NL_TemporallyBruteforced(data)
+        modelDict = Playbook.optimize_MPOPF_1ph_NL_TemporallyBruteforced(data)
     elseif linearizedModel
-        modelDict = optimize_MPOPF_1ph_L(data)
+        modelDict = Playbook.optimize_MPOPF_1ph_L(data)
     else
         error("linearizedModel must be either true or false")
     end
     
     @unpack model, modelVals, data = modelDict
     # print_mu(modelDict)
-
 elseif temporal_decmp
-    modelDict = optimize_MPOPF_1ph_NL_DDP(data, maxiter=maxiter_ddp)
+    modelDict = Playbook.optimize_MPOPF_1ph_NL_DDP(data, maxiter=maxiter_ddp)
     @unpack modelVals, data = modelDict
     # print_mu(modelDict)
 else
@@ -103,8 +99,9 @@ else
 end
 
 if temporal_decmp
-    plot_substation_power_cost_allT_vs_k(modelDict, savePlots=savePlots)
+    Plotter.plot_substation_power_cost_allT_vs_k(modelDict, savePlots=savePlots)
 end
+#endregion
 
 # postsim computation, plotting, logging
 begin
@@ -112,25 +109,25 @@ begin
     verbose = false
     # verbose = true
 
-    modelDict = compute_output_values(modelDict, verbose=verbose)
+    modelDict = CO.compute_output_values(modelDict, verbose=verbose)
 
-    export_decision_variables(modelDict, verbose=verbose)
+    Exporter.export_decision_variables(modelDict, verbose=verbose)
 
     # Todo: Maybe separately save the simulation times? It is annoying to have file content differences every single run (for same exact sim)
-    export_simulation_key_results_txt(modelDict, verbose=verbose)
+    Exporter.export_simulation_key_results_txt(modelDict, verbose=verbose)
 
-    plot_battery_actions(modelDict, showPlots=false, savePlots=savePlots, verbose=verbose, maxBatts=20)
+    Plotter.plot_battery_actions(modelDict, showPlots=false, savePlots=savePlots, verbose=verbose, maxBatts=20)
 
     @unpack data = modelDict;
-    plot_input_forecast_curves(data, filenameSuffix=inputForecastDescription, showPlots=false, verbose=verbose)
-    plot_substation_power(modelDict, savePlots=savePlots, verbose=verbose)
-    plot_substation_power_cost(modelDict, savePlots=savePlots, verbose=verbose)
-    plot_line_losses(modelDict, savePlots=savePlots, verbose=verbose)
+    Plotter.plot_input_forecast_curves(data, filenameSuffix=inputForecastDescription, showPlots=false, verbose=verbose)
+    Plotter.plot_substation_power(modelDict, savePlots=savePlots, verbose=verbose)
+    Plotter.plot_substation_power_cost(modelDict, savePlots=savePlots, verbose=verbose)
+    Plotter.plot_line_losses(modelDict, savePlots=savePlots, verbose=verbose)
 
     modelDict = validate_opf_against_opendss(modelDict, verbose=verbose)
     @unpack valdVals = modelDict;
-    export_validation_key_results(modelDict, verbose=verbose)
+    Exporter.export_validation_key_results(modelDict, verbose=verbose)
 
-    export_validation_decision_variables(modelDict, verbose=verbose)
+    Exporter.export_validation_decision_variables(modelDict, verbose=verbose)
 end
 
