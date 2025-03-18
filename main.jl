482f545e0dec70ee961cdf8ef650298e22ee26ea
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2025-03-18 09:38:25 -0700

    m

--- main.jl
+++ main.jl
@@ -24,7 +24,7 @@ end;
 begin
     T = Int(T0*factor) 
     numAreas = 1
-    maxiter_ddp = 7
+    maxiter_ddp = 25
     # objfun0 = "lineLossMin"
     objfun0 = "subsPowerCostMin"
     objfun2 = "scd"
