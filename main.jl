a2120235e1778dfcb76f37c0045fc8dc4024703e
= <aryan.r.jha@gmail.com>
2025-04-01 05:45:17 -0700

    hmm

--- main.jl
+++ main.jl
@@ -16,9 +16,9 @@ begin
     factor = 1
     # factor = 1/2
     linearizedModel = false
-    linearizedModel = true
+    # linearizedModel = true
     temporal_decmp = false
-    # temporal_decmp = true
+    temporal_decmp = true
     warmStart_mu = "none"
     # warmStart_mu = "nonlinear"
     # warmStart_mu = "linear"
