20c7be0726a0f233ede81f3621fdb79411a02d7e
= <aryan.r.jha@gmail.com>
2025-04-04 22:59:17 -0700

    m

--- main.jl
+++ main.jl
@@ -3,16 +3,16 @@ include("./src/setupMultiPeriodDistOPF.jl")
 Revise.track(MultiPeriodDistOPF.DDP)
 
 begin
-    # systemName0 = "ads3_1ph"
+    systemName0 = "ads3_1ph"
     # systemName0 = "ads10_1ph"
     # systemName0 = "ieee123_1ph-A"
-    systemName0 = "ieee123_1ph-B"
+    # systemName0 = "ieee123_1ph-B"
     # systemName0 = "ieee729_1ph"
     # systemName0 = "ieee730_1ph"
     # T0 = 8
     # T0 = 1
-    T0 = 96
-    # T0 = 3
+    # T0 = 96
+    T0 = 3
     # T0 = 6
     # T0 = 12
     # T0 = 24
