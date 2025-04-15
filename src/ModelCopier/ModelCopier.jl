4541edf074c8618c7346094690fb98c28f26ed8d
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2025-04-15 09:16:06 -0700

    m

--- src/ModelCopier/ModelCopier.jl
+++ src/ModelCopier/ModelCopier.jl
@@ -227,7 +227,7 @@ function store_FP_k_decvar_values(ddpModel;
 
     modelVals_ddp_vs_FP[k_ddp] = modelVals_k0
 
-    modelVals = modelVals_ddp_vs_FP_k0    
+    modelVals = modelVals_k0
     @pack! ddpModel = modelVals_ddp_vs_FP, modelVals
     return ddpModel
 end
