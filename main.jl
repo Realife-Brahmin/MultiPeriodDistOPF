060b7bbb6cb5ec0ec5b04d62d0523937f74349ae
= <aryan.r.jha@gmail.com>
2025-05-06 12:45:44 -0700

    Even my relaxed heuristics don't save 123B from non-convergence

--- main.jl
+++ main.jl
@@ -35,7 +35,7 @@ begin
     # alpha_fpi = 0.001
     T = Int(T0*factor) 
     numAreas = 1
-    maxiter_ddp = 25
+    maxiter_ddp = 50
     # objfun0 = "lineLossMin"
     objfun0 = "subsPowerCostMin"
     objfun2 = "scd"
