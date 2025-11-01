a7d80a9d3219598a334fb70c70894043bc6fb639
= <aryan.r.jha@gmail.com>
2025-11-01 14:02:49 -0700

    Implement code changes to enhance functionality and improve performance

--- tadmm_socp.jl
+++ tadmm_socp.jl
@@ -71,7 +71,7 @@ includet(joinpath(env_path, "Plotter.jl"))
 # System and simulation parameters
 # systemName = "ads10A_1ph"
 systemName = "ieee123A_1ph"
-T = 24  # Number of time steps
+T = 96  # Number of time steps
 delta_t_h = 24.0/T  # Time step duration in hours
 
 # Solver selection
