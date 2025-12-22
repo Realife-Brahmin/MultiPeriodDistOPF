0ec459414f237321f617c2354cd514e1ee67dc4a
= <aryan.r.jha@gmail.com>
2025-12-22 07:46:07 -0800

    better for now

--- tadmm_socp.jl
+++ tadmm_socp.jl
@@ -86,10 +86,10 @@ includet(joinpath(env_path, "Plotter.jl"))
 # System and simulation parameters
 # systemName = "ads10A_1ph"
 systemName = "ieee123A_1ph"
-# T = 24  # Number of time steps
+T = 24  # Number of time steps
 # T = 48  # Number of time steps
 # T = 96  # Number of time steps
-T = 480  # Number of time steps
+# T = 480  # Number of time steps
 # T = 144
 delta_t_h = 24.0/T  # Time step duration in hours
 
